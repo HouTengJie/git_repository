45228a5cf25dea7a5f4325490b550b51e61b2c24
HouTengJie <65161193+HouTengJie@users.noreply.github.com>
2020-05-13 17:49:55 +0800

    Update Test1.java

--- Test1/src/main/java/com/usian/java/Test1.java
+++ Test1/src/main/java/com/usian/java/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
     public static void main(String[] args) {
         //其他
         System.out.println("12345");
+        //github(其他同事)端的更新
         System.out.println("678910");
     }
 }
